0e5985dfc15c3d97e2d19a07fd7ab8f26124bc7f
Jesn <48741820+onebtcdesign@users.noreply.github.com>
2025-11-24 23:52:51 +0800

    添加配置文件

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        apple: {
+          gray: '#F5F5F7',
+          blue: '#007AFF',
+          hoverBlue: '#0062CC',
+          border: '#D2D2D7',
+          text: '#1D1D1F',
+          subtext: '#86868B'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
